1d279d7b3bacf7e6a6f6c197a196038f075fda4b
github <694728793@qq.com>
2018-06-24 23:34:22 +0800

    sprint jpa

--- src/main/java/com/south/oly/Configure.java
+++ src/main/java/com/south/oly/Configure.java
@@ -1,8 +1,10 @@
 package com.south.oly;
 
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.ComponentScan;
 
+@Configurable
 @ComponentScan(basePackages = {"com.south.oly"})
 public class Configure {
 
